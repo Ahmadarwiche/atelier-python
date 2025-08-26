e8550ac0d22c312bfc7351562874d5a4cfaf6899
Ahmadarwiche <darwich.ahmad.92@gmail.com>
2025-08-26 12:14:13 +0200

    feat: :construction: chap 16 les bonnes pratiques in progress

---
+++ src/utils/script_quality_not_ok.py
@@ -0,0 +1,29 @@
+"""Un script de multiplication.
+"""
+
+import os
+
+def Multiplie_nombres(nombre1,nombre2 ):
+    """Multiplication de deux nombres entiers
+    Cette fonction ne sert pas Ã  grand chose.
+
+    Parameters
+    ----------
+    nombre1 : int
+        Le premier nombre entier.
+    nombre2 : int
+        Le second nombre entier.
+        TrÃ¨s utile.
+
+    Returns
+    -------
+    int
+        Le produit des deux nombres.
+
+    """
+    return nombre1 *nombre2
+
+
+if __name__ == "__main__":
+    print(f"2 x 3 = {Multiplie_nombres(2, 3)}")
+    print (f"4 x 5 = {Multiplie_nombres(4, 5)}")
